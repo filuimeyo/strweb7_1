6edf839b405284936575a3e964f6b56dd615a727
filuimeyo <nikakud24@gmail.com>
2023-11-15 01:05:53 +0300

    strweb7_1_2.1.3.05

--- src/main/java/by/iba/filter/LoginRequiredFilter.java
+++ src/main/java/by/iba/filter/LoginRequiredFilter.java
@@ -1,40 +1,49 @@
 package by.iba.filter;
 
+import by.iba.command.session.SessionAttribute;
+import by.iba.util.pages.Page;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
+@WebFilter(urlPatterns = "/controller")
 public class LoginRequiredFilter implements Filter {
+    private static final String COMMAND = "command";
+    private static final String WELCOME = "welcome";
+    private static final String ERROR_MESSAGE = "error_message";
+    private static final String ERROR_TEXT = "Нет авторизации для выполнения данной команды";
+    private static final Logger logger = LogManager.getLogger(LoginRequiredFilter.class.getName());
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-
-        HttpServletRequest httpReq = (HttpServletRequest) request;
-        HttpServletResponse httpResp = (HttpServletResponse) response;
-        HttpSession session = httpReq.getSession();
-        if (session.getAttribute("name")!=null) {
-            chain.doFilter(request, response);
-        }
-        else {
-
-// httpResp.sendRedirect(httpReq.getContextPath() + "/LoginServlet");
-            session.invalidate();
-            request.getRequestDispatcher("LoginServlet").forward(request,
-                    response);
-        }
 
     }
 
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String command = request.getParameter(COMMAND);
+        logger.info("Filter is working " + COMMAND + "= " + command);
+        if (!command.equals(WELCOME)) {
+            chain.doFilter(req, resp);
+        } else {
+            if (request.getSession().getAttribute(SessionAttribute.NAME) != null) {
+                chain.doFilter(req, resp);
+            } else {
+                request.setAttribute(ERROR_MESSAGE, ERROR_TEXT);
+                request.getRequestDispatcher(Page.ERROR_PAGE.getPage()).forward(req, resp);
+            }
+        }
+    }
 
     @Override
     public void destroy() {
-        Filter.super.destroy();
+
     }
 }
+
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,3 +1,4 @@
+<%--
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -22,6 +23,28 @@
             </div>
             <div>
                 <a href="RegisterServlet">Регистрация</a>
+            </div>
+        </form>
+    </section>
+</div>
+</body>
+</html>
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>Title</title></head>
+<body>
+<div class="container">
+    <section id="content"><p><font color="red">${errorMessage}</font></p>
+        <form action="${pageContext.servletContext.contextPath}/controller?command=login" method="POST"><h1> Вход в
+            систему</h1>
+            <div><input placeholder="Имя" required="" id="username" name="loginName" type="text"/></div>
+            <div><input placeholder="Пароль" required="" id="password" name="password" type="password"/></div>
+            <div><input type="submit" value="Войти"/></div>
+            <div>
+                <a href="${pageContext.servletContext.contextPath}/controller?command=registration_page">Регистрация</a>
             </div>
         </form>
     </section>
